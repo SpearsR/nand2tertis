fe4c69d6f4976cf9568a00716bc879373cc45627
SpearsR <18rspears@gmail.com>
2021-04-20 14:05:52 -0600

    4th Project

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -10,5 +10,82 @@
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
-
 // Put your code here.
+
+@8192
+D=A
+@R0
+M=D //R0 = 500
+
+@16384
+D=A
+@R1
+M=D //R1 = 16384
+
+@i
+M=-1 //i = 0
+
+(ZERO)
+@i
+M=0
+@ZEROD
+0;JMP  //zeros i then returns to loop
+
+(LOOP)
+@ZERO
+0;JMP
+(ZEROD)
+@24576 
+D=M //Stores keycode in D
+@CHANGEW
+D;JEQ
+@CHANGEB
+D;JGT
+@LOOP 
+0;JMP
+
+(CHANGEW)
+@R1
+D=M
+@i
+A=D+M //Goes to new mem locationg to change pixels
+
+M=0 //Sets all 16 bits to 'white'
+
+@i
+M=M+1 //i++
+
+@R0
+D=M
+@i
+D=D-M  //500 - i
+@CHANGEW
+D;JGT //goto change if 500-i>0
+@LOOP
+0;JMP
+
+(CHANGEB)
+@R1
+D=M
+@i
+A=D+M //Goes to new mem locationg to change pixels
+
+M=-1 //Sets all 16 bits to 'white'
+
+@i
+M=M+1 //i++
+
+@R0
+D=M
+@i
+D=D-M  //500 - i
+@CHANGEB
+D;JGT //goto change if 500-i>0
+@LOOP
+0;JMP
+
+
+
+@LOOP
+0;JMP   
+
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -10,3 +10,44 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+@R0
+D=M
+@num1
+M=D  //num1 = R0
+
+@R1
+D=M
+@num2
+M=D  //num2 = R1
+@num2
+D=M
+D=D-1
+@n
+M=D   //n = num2 - 1
+
+@R2
+M=0 //R2 = 0
+
+@i
+M=0  //i = 0
+
+(LOOP)
+@n
+D=M
+@i
+D=D-M
+@STOP
+D;JLT // if i > n goto STOP
+@R0
+D=M
+@R2
+M=D+M  // num1 + total
+@i
+M=M+1
+@LOOP
+0;JMP
+(STOP)
+
+(END)
+@END
+0;JMP 
